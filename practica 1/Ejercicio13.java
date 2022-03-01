79ea5f7f2ed9536e007e074eea678b096ce47e40
Adriviciano <97399669+Adriviciano@users.noreply.github.com>
2022-03-01 11:56:22 +0100

    Add files via upload

---
+++ practica 1/Ejercicio13.java
@@ -0,0 +1,32 @@
+public class Ejercicio13 {
+   public static String obtenerPalabra(String cad, int num){
+      if (num < 1 || num >= Ejercicio12.contarPalabras(cad)){
+         return null;
+      }
+
+      int posicion = 1;
+      String espacio = " ";
+      for (int i = 0; i<cad.length(); i++) {
+         if (cad.charAt(i) == espacio.charAt(0)){
+            posicion += 1;
+         }
+         if (posicion == num){
+            String palabra = "";
+            int j = i + 1;
+            while (cad.charAt(j) != espacio.charAt(0)){
+               palabra = palabra + cad.charAt(j);
+               j += 1;
+            }
+            return palabra;
+         }
+      }
+
+   }
+
+   public static void main(String[] args) {
+      System.out.println(obtenerPalabra("Uno dos tres cuatro cinco seis siete ocho nueve", 7) + " = 8?");
+      System.out.println(obtenerPalabra("Uno dos tres cuatro cinco seis siete ocho nueve", 2) + " = 3?");
+      System.out.println(obtenerPalabra("Uno dos tres cuatro cinco seis siete ocho nueve", -1) + " = null?");
+      System.out.println(obtenerPalabra("Uno dos tres cuatro cinco seis siete ocho nueve", 9) + " = null?");
+   }
+}
